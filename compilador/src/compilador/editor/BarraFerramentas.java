15f032ef7eb15b8a25f20b160ce430e63bced62b
João da Silva e Souza <joao8816@outlook.com>
2023-05-02 12:03:23 -0300

    ajustando editor para não mostrar lista de tokens...
    ... conforme pedido na especificação da parte 3 do trabalho.

--- compilador/src/compilador/editor/BarraFerramentas.java
+++ compilador/src/compilador/editor/BarraFerramentas.java
@@ -153,71 +153,16 @@ public class BarraFerramentas {
         Token t = null;
         try {
             while ((t = lexico.nextToken()) != null) {
-                int indice = editor.getText().indexOf(t.getLexeme());
-                int linha = editor.getLineOfOffset(indice) + 1;
-
-                switch (t.getId()) {
-                    case 2:
-                        resultado += linha + " identificador " + t.getLexeme();
-                    break;
-                    case 3:
-                        resultado += linha + " constante_int " + t.getLexeme();
-                    break;
-                    case 4:
-                        resultado += linha + " constante_float " + t.getLexeme();
-                    break;
-                    case 5:
-                        resultado += linha + " constante_binaria " + t.getLexeme();
-                    break;
-                    case 6:
-                        resultado += linha + " constante_string " + t.getLexeme();
-                    break;
-                    case 7:
-                    case 8:
-                    case 9:
-                    case 10:
-                    case 11:
-                    case 12:
-                    case 13:
-                    case 14:
-                    case 15:
-                    case 16:
-                    case 17:
-                    case 18:
-                        resultado += linha + " palavra reservada " + t.getLexeme();
-                        break;
-                    case 19:
-                    case 20:
-                    case 21:
-                    case 22:
-                    case 23:
-                    case 24:
-                    case 25:
-                    case 26:
-                    case 27:
-                    case 28:
-                    case 29:
-                    case 30:
-                    case 31:
-                    case 32:
-                    case 33:
-                    case 34:
-                        resultado += linha + " simbolo especial " + t.getLexeme();
-                        break;
-                }
-
-                resultado += "\n";
-
+                // Não faz nada?
             }
-            resultado += "\nPrograma compilado com sucesso.";
+            resultado += "Programa compilado com sucesso.";
         } catch (LexicalError e) { // tratamento de erros
             int indice = editor.getText().indexOf(e.getLexema());
             int linha = editor.getLineOfOffset(indice) + 1;
             resultado = "Erro na linha " + linha + " - " + e.getLexema() + " " + e.getMessage();
 
-        } catch (BadLocationException e) {
-            throw new RuntimeException(e);
         }
+
         return resultado;
     }
 
