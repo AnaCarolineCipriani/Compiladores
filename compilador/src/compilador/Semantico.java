14c0db82ce3e8c58bb8d54e392e9ed6a6a3a1ebc
Arleca <carolsantos2002@gmail.com>
2023-05-29 21:13:02 -0300

    Início da implementação das ações semânticas

--- compilador/src/compilador/Semantico.java
+++ compilador/src/compilador/Semantico.java
@@ -1,9 +1,34 @@
 package compilador;
 
-public class Semantico implements Constants
-{
-    public void executeAction(int action, Token token)	throws SemanticError
-    {
-        System.out.println("Ação #"+action+", Token: "+token);
-    }	
+import java.util.Stack;
+
+public class Semantico implements Constants {
+	
+	private String operador = "";
+	private String codigo;
+	private Stack<String> pilhaTipos;
+
+	public void executeAction(int action, Token token) throws SemanticError {
+		System.out.println("Ação #" + action + ", Token: " + token);
+	}
+    
+    public void doAcaoSemantica1() {
+    	String tipo1 = pilhaTipos.peek();
+    	String tipo2 = pilhaTipos.peek();
+    	
+    	String tipoFloat = "float64";
+    	String tipoInt = "int64";
+    	
+		if (tipo1 == tipoFloat || tipo2 == tipoFloat) {
+    		pilhaTipos.push(tipoFloat);
+    	} else {
+    		pilhaTipos.push(tipoInt);
+    	}
+    	// TODO codigo.adiciona(add);
+
+    }
+    
+    public void doAcaoSemantica2() {
+    	
+    }
 }
